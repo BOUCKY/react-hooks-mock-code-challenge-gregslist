16ebc09e7953375e81162f91e1b3984e9d5fbe70
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-06-20 12:58:05 -0400

    First two deliverables (getting all cards to show on page load + favorite button)

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+// We pass the image, description and location props in from the ListingsContainer so the ListingCard has access to the data.
+function ListingCard({image, description, location}) {
+
+  // use useState to toggle the favorite button
+  // variable = favorite, function = setFavorite
+  // state is initially set to false because we don't want any of the    listings to be favorited when the page loads.
+  const [favorite, setFavorite] = useState(false)
+  
+  // function toggles the state of favorite.
+  const handleFavorite = () => {
+    setFavorite(!favorite)
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        {/* Passing in image prop as the img src value */}
+        <img src={image} alt={"description"} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {/* set the boolean (typically true or false) to the favorite variable which is initially set to false, but will change when the button is clicked*/}
+        {favorite ? (
+          // pass in the toggle handleFavorite function on click
+          <button className="emoji-button favorite active" onClick={handleFavorite}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          // pass in the toggle handleFavorite function on click
+          <button className="emoji-button favorite" onClick={handleFavorite} >☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        {/* Passing in description prop as the <strong> description value */}
+        <strong>{description}</strong>
+        {/* Passing in description prop as the <span> location value */}
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
@@ -22,3 +40,18 @@ function ListingCard() {
 }
 
 export default ListingCard;
+
+// You can put two separate functions for the toggle because if the request is taking too long to load, and the user presses the favorite button more than once, we don't want it to keep toggling between true and false while the request is loading, we want the state to stay the same until the request loads.
+
+// You should also disable the button until the state changes so the user can't keep pressing the button while the request is loading so the don't cause any weird errors.
+
+  // const handleFavorite = () =>{
+  //   setFavorite(true)
+  // }
+  // const handleUnFavorite = () =>
+  //   setFavorite(false)
+
+// This button will use the handleUnFavorite function because if the star is filled, that means the ListingCard is already favorited so we want to unfavorite it when we click the filled star
+  // <button className="emoji-button favorite active" onClick={handleUnFavorite}>★</button>
+// This button will use the handleFavorite function because if the star is not filled, that means the ListingCard is already unfavorited so we want to favorite it when we click the unfilled star
+  // <button className="emoji-button favorite" onClick={handleFavorite} >☆</button>
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React,{useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
 function ListingsContainer() {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    // This is saying the response from the FETCH request = the json data
+    .then((response) => response.json())
+    // This is taking in what the response.json is returning as 'data'
+    .then((data) => setListings(data))
+    
+  // useEffect needs an empty array as a second argument. Empty array = useEffect will only call the function when the page first loads.
+  }, [])
+
+  // map through the data, give each ListingCard a value for key, image, description, and location based on each object in the data.
+  const renderListings = listings.map((dataObject) => 
+    <ListingCard
+    // passing these values as props. Left side of = is the prop. the right side of the = is the value of said prop.
+    key={dataObject.id}
+    image={dataObject.image}
+    description={dataObject.description}
+    location={dataObject.location}
+    />
+  )
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {/* Rather than passing in <ListingCard />, we pass in the renderListings variable that contains the map and all of the props for <ListingCard /> */}
+        {renderListings}
       </ul>
     </main>
   );
